8b696502c437807c4b571359ed48105eb5720705
Xin <147829973+xinchar@users.noreply.github.com>
2023-10-15 02:09:21 +0800

    feat: change date extraction in script

--- get_rootfs.sh
+++ get_rootfs.sh
@@ -9,6 +9,12 @@ URL="https://images.linuxcontainers.org/images/debian/bookworm/amd64/default/"
 # Get the latest timestamp directory
 LATEST_FOLDER=$(curl -s $URL | grep -o 'href=".*"' | awk -F\" '{print $2}' | tail -n 1)
 
+# Extract the date part from the folder name using awk
+DATE_ONLY=$(echo $LATEST_FOLDER | awk -F "/" '{print $1}' | awk -F "_" '{print $1}')
+
+# Save version into release.txt
+echo $DATE_ONLY > release.txt
+
 # Concatenate URL and filename
 FILE_URL="${URL}${LATEST_FOLDER}rootfs.tar.xz"
 
